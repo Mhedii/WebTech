99a1708e08edaeadcacbbf85914bf78535fb25ef
Mhedii <61927269+Mhedii@users.noreply.github.com>
2021-02-05 16:22:44 +0600

    Add files via upload

---
+++ LabTask1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+<title>LabTask!</title>
+</head>
+
+
+<body>
+<h1>REGISTRATION</h1>
+
+<form>
+
+<table>
+<tr>
+<td>Name:</td><td><input type="text" id="name" name="name"></td>
+</tr>
+<tr>
+<td>Email:</td><td><input type="text" id="email" name="email"></td>
+</tr>
+<tr>
+<td>User Name:</td><td><input type="text" id="user" name="user"></td>
+</tr>
+<tr>
+<td>Password:</td><td><input type="password" id="p" name="password"></td>
+</tr>
+<td>Confirm Password:</td><td><input type="cpassword" id="p" name="cpassword"></td>
+</tr>
+</table>
+Gender<br>
+<input type="radio"id="male" name="gender" value="male">Male
+<input type="radio"id="female" name="gender" value="female">Female
+<input type="radio"id="other" name="gender" value="other">Other
+<br>
+Date Of Birth
+<input type="date"id="date" name="date"><br>
+<input type="submit"value="Submit">
+<input type="reset"value="Reset">
+
+</form>
+
+</body>
+</html>
